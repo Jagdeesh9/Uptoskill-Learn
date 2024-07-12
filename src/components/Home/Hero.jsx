a35bb323342f82e7d6df71820f48f9a58aeb2e47
vaishnavigupta122 <vaishnavigupta729@gmail.com>
2024-07-12 20:14:41 +0530

    green

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
         <h2 className='text-3xl lg:text-5xl text-center font-bold mb-2'>Learning to code got simplified.</h2>
         <p className='text-center text-xl'>Learn the latest technology with interactive, hands-on courses. <span className='font-bold'>It’s free.</span></p>
 
-        <div className='w-full flex items-center justify-center'><i></i><button className='py-2 w-full md:w-96 lg:w-96 lg:px-10 p-6 bg-blue-500 rounded-sm font-bold mt-10'>Log In with google </button></div>
+        <div className='w-full flex items-center justify-center'><i></i><button className='py-2 w-full md:w-96 lg:w-96 lg:px-10 p-6 bg-green-500 rounded-sm font-bold mt-10'>Log In with google </button></div>
         <span className='mt-3'>__________________<span className='text-xl uppercase'> or </span>__________________</span>
         <button className='py-2 w-full lg:w-96 md:w-96 lg:px-10 p-6 border border-blue-500 rounded-sm font-bold mt-3'>See more options</button>
     </div>
